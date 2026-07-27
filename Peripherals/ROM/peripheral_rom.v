d4478c4392ba765c704080efd53b8d10fbde4585
SaiManojGubbala <saimanojgubbala@gmail.com>
2026-07-28 00:12:55 +0530

    instuction memory code updated

---
+++ Peripherals/ROM/peripheral_rom.v
@@ -0,0 +1,31 @@
+`define WIDTH 31:0
+`define MEMDEPTH 0:16383
+`define ZEROWORD 32'h0
+`define MEMADDRESS 15:2
+// ROM - INSTRUCTION MEMORY
+module instruction_memory(
+    clk, reset, write_enable_in, address_in, data_in, data_out
+);
+
+//inputs/outputs from slave bus
+input clk, reset, write_enable_in;
+input [`WIDTH] data_in, address_in;
+output reg [`WIDTH] data_out;
+
+// memory defining
+reg [31:0] MEMORY [`MEMDEPTH];
+
+// asynchronous read
+always @(*) begin
+    if(reset == 1'b1) begin
+        data_out = `ZEROWORD;
+    end
+    else data_out = MEMORY[address_in[`MEMADDRESS]];
+end
+
+//synchronous write
+always @(posedge clk) begin
+    if(write_enable_in == 1'b1) MEMORY[address_in[`MEMADDRESS]] = data_in;
+end
+
+endmodule
